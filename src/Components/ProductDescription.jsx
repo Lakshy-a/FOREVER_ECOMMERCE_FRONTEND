a1d9b6ca20aff4b63e6a0b1d7e54dedaafe1f61a
Lakshya_Gupta <lakshyagupta8786@gmail.com>
2024-10-04 10:59:52 +0530

    Product page design complete

--- src/Components/ProductDescription.jsx
+++ src/Components/ProductDescription.jsx
@@ -1,33 +1,47 @@
 import React from "react";
 import star_dull_icon from "../assets/frontend_assets/star_dull_icon.png";
+import star_icon from "../assets/frontend_assets/star_icon.png";
+import DescriptionReviews from "./DescriptionReviews";
 const ProductDescription = ({ neededProduct }) => {
   return (
     <>
-      <div className="flex flex-col gap-7">
+      <div className="flex flex-col gap-2 border-b pb-4 ">
         <h1 className="text-2xl font-semibold mt-4">{neededProduct.name}</h1>
-        <div>
-          <img src={star_dull_icon} className="w-3" />
+        <div className="flex gap-2 items-center">
+          <img src={star_icon} className="w-3 h-3" />
+          <img src={star_icon} className="w-3 h-3" />
+          <img src={star_icon} className="w-3 h-3" />
+          <img src={star_icon} className="w-3 h-3" />
+          <img src={star_dull_icon} className="w-3 h-3" />
+          <div>(122)</div>
         </div>
-        <div className="text-3xl font-semibold">${neededProduct.price}</div>
-        <p className="text-medium font-normal text-gray-500">
+        <div className="text-3xl font-semibold mt-4">
+          ${neededProduct.price}
+        </div>
+        <p className="text-medium font-normal text-gray-500 mt-4">
           {neededProduct.description}
         </p>
         <div>
-          <h2 className="font-medium">Select Size</h2>
+          <h2 className="font-medium mt-4">Select Size</h2>
           <ul className="flex mt-4 gap-7">
             {neededProduct.sizes.map((size, index) => (
               <li
                 key={index}
-                className="border border-gray-300 px-2 py-1 cursor-pointer bg-slate-200 "
+                className="border border-gray-200 px-3 py-1 cursor-pointer bg-slate-200 "
               >
                 {size}
               </li>
             ))}
           </ul>
         </div>
-        <button className="uppercase bg-black text-white px-6 py-3 w-fit hover:text-black hover:bg-white hover:border border-black">
+        <button className="mt-8 uppercase bg-black text-white px-6 py-3 w-fit hover:text-black hover:bg-white hover:border border-black">
           Add To Cart
         </button>
+      </div>
+      <div className="mt-4 text-sm text-gray-500">
+        <h2 className="mt-1">100% Original product.</h2>
+        <h2 className="mt-1">Cash on delivery is available on this product.</h2>
+        <h2 className="mt-1">Easy return and exchange policy within 7 days.</h2>
       </div>
     </>
   );
--- src/Components/ProductPage.jsx
+++ src/Components/ProductPage.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import MoreImages from "./MoreImages";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-import ProductDescription from "./ProductDescription"
+import ProductDescription from "./ProductDescription";
+import DescriptionReviews from "./DescriptionReviews";
 
 const ProductPage = () => {
   const products = useSelector((state) => state.products.products);
@@ -16,11 +17,18 @@ const ProductPage = () => {
 
   return (
     <>
-      <div className="custom-padding mt-8 flex flex-col xs:flex-row gap-12">
-        <div className="flex flex-col gap-4">
-          <MoreImages images={neededProduct.image} />
+      <div className="custom-padding mt-8">
+        <div className=" flex flex-col xs:flex-row gap-12">
+          <div className="flex flex-col gap-4">
+            <MoreImages images={neededProduct.image} />
+          </div>
+          <div>
+            <ProductDescription neededProduct={neededProduct} />
+          </div>
         </div>
-        <div><ProductDescription neededProduct={neededProduct} /></div>
+        <div className="mt-16">
+          <DescriptionReviews />
+        </div>
       </div>
     </>
   );
